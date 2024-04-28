5f0051900687e7b08dbe7d1a86e7db423741ff9f
Jason Yuan <jy469@login01.chn.perlmutter.nersc.gov>
2024-04-28 14:25:10 -0700

    added hw1 starter code

---
+++ build/CMakeFiles/benchmark-blas.dir/dgemm-blas.c.o.d
@@ -0,0 +1,5 @@
+CMakeFiles/benchmark-blas.dir/dgemm-blas.c.o: \
+ /global/homes/j/jy469/cs5220/CS5220Project/dgemm-blas.c \
+ /usr/include/stdc-predef.h \
+ /opt/cray/pe/libsci/23.12.5/GNU/12.3/x86_64/include/cblas.h \
+ /usr/lib64/gcc/x86_64-suse-linux/12/include/stddef.h
---
+++ build/CMakeFiles/benchmark-naive.dir/dgemm-naive.c.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/benchmark-naive.dir/dgemm-naive.c.o: \
+ /global/homes/j/jy469/cs5220/CS5220Project/dgemm-naive.c \
+ /usr/include/stdc-predef.h
