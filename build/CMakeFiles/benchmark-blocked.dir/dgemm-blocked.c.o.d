b85036af6be2ac579658fdae3df7bc925d9dd5e0
Jason Yuan <jy469@login01.chn.perlmutter.nersc.gov>
2024-04-29 12:26:48 -0700

    added gitignore, removed build

--- build/CMakeFiles/benchmark-blocked.dir/dgemm-blocked.c.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/benchmark-blocked.dir/dgemm-blocked.c.o: \
- /global/homes/j/jy469/cs5220/CS5220Project/dgemm-blocked.c \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/benchmark-naive.dir/dgemm-naive.c.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/benchmark-naive.dir/dgemm-naive.c.o: \
- /global/homes/j/jy469/cs5220/CS5220Project/dgemm-naive.c \
- /usr/include/stdc-predef.h
