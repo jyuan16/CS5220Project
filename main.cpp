cf1809e2bcf24ef299cab1bf58febd7a0881bbfe
Bryan Lee <bdl55@login25.chn.perlmutter.nersc.gov>
2024-04-30 11:34:06 -0700

    cleaned up main.cpp
    -added -d flag for number of days
    -also added lines for openmp.cpp in CMakeLists.txt

--- main.cpp
+++ main.cpp
@@ -7,56 +7,6 @@
 #include <random>
 #include <vector>
 
-// =================
-// Helper Functions
-// =================
-
-// I/O routines
-// void save(std::ofstream &fsave, person_t *parts, int num_parts, double size)
-// {
-//     static bool first = true;
-
-//     if (first)
-//     {
-//         fsave << num_parts << " " << size << "\n";
-//         first = false;
-//     }
-
-//     for (int i = 0; i < num_parts; ++i)
-//     {
-//         fsave << parts[i].x << " " << parts[i].y << "\n";
-//     }
-
-//     fsave << std::endl;
-// }
-
-// Particle Initialization
-void init_particles(int num_people, int part_seed, std::queue<double> *entry)
-{
-    std::random_device rd;
-    std::mt19937 gen(part_seed ? part_seed : rd());
-
-    std::vector<int> shuffle(num_people);
-    for (int i = 0; i < shuffle.size(); ++i)
-    {
-        shuffle[i] = i;
-    }
-    double time = 0;
-    for (int i = 0; i < num_people; ++i)
-    {
-        // Make sure particles are not spatially sorted
-        std::uniform_int_distribution<int> rand_int(0, num_people - i - 1);
-        int j = rand_int(gen);
-        int k = shuffle[j];
-        shuffle[j] = shuffle[num_people - i - 1];
-
-        // Distribute particles evenly to ensure proper spacing
-        std::exponential_distribution<> rand_time(10);
-        time += rand_time(gen);
-        entry->push(time);
-    }
-}
-
 // Command Line Option Processing
 int find_arg_idx(int argc, char **argv, const char *option)
 {
@@ -105,29 +55,16 @@ int main(int argc, char **argv)
     {
         std::cout << "Options:" << std::endl;
         std::cout << "-h: see this help" << std::endl;
-        std::cout << "-n <int>: set number of particles" << std::endl;
-        std::cout << "-o <filename>: set the output file name" << std::endl;
-        std::cout << "-s <int>: set particle initialization seed" << std::endl;
+        std::cout << "-d: <int>: set end time (in seconds)" << std::endl;
         return 0;
     }
 
-    // Open Output File
-    char *savename = find_string_option(argc, argv, "-o", nullptr);
-    std::ofstream fsave(savename);
-
     // Initialize Particles
-    int num_parts = find_int_arg(argc, argv, "-n", 1000);
-    int part_seed = find_int_arg(argc, argv, "-s", 0);
-    int end = 24 * 60 * 60;
-
-    std::exponential_distribution<> entry_time(10);
-
-    // init_particles(num_parts, part_seed, entry_time);
+    int end = find_int_arg(argc, argv, "-d", 1) * (24 * 60 * 60);
 
     // Algorithm
     auto start_time = std::chrono::steady_clock::now();
 
-    // init_simulation(entry_rate, check_in_time, bag_check_time, security_time, precheck_time, prob_precheck, prob_check_in, prob_check_bag, num_check_in, num_bag_check, num_security, num_precheck);
     init_simulation();
     double current_time = 0;
 
@@ -136,18 +73,8 @@ int main(int argc, char **argv)
 #endif
     {
         while (current_time < end)
-        // for (int step = 0; step < 1; ++step)
         {
             current_time = simulate_one_step(current_time);
-
-            // Save state if necessary
-#ifdef _OPENMP
-#pragma omp master
-#endif
-            // if (fsave.good() && (step % savefreq) == 0)
-            // {
-            //     save(fsave, parts, num_parts, size);
-            // }
         }
     }
 
@@ -157,7 +84,5 @@ int main(int argc, char **argv)
     double seconds = diff.count();
 
     // Finalize
-    std::cout << "Simulation Time = " << seconds << " seconds for " << num_parts << " particles.\n";
-    fsave.close();
-    // delete[] parts;
+    std::cout << "Simulation Time = " << seconds << " seconds for " << end / (24 * 60 * 60) << " days.\n";
 }
